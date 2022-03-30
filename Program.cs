c0f7701df3cb654c41c2d018e7065a92b38dee2a
student 04 <stk_lum_04@outlook.de>
2022-03-30 08:48:18 +0200

    string

--- Program.cs
+++ Program.cs
@@ -22,47 +22,60 @@ namespace strings
                 Console.WriteLine("3:Beenden");
 
                 Console.WriteLine("Aaswahl:");
-                auswahl=Convert:Toint32(Console.ReadLine());
+                auswahl=Convert.ToInt32(Console.ReadLine());
 
                 switch(auswahl)
                 {
-                    case"1";
+                    case 1:
                 Console.WriteLine("1:zelchen ersetzen.");
-                        String zuErsetzen, zeichnen, erzatz;
+                        String zuErsetzen, zeichnen, ersatz;
                         Console.WriteLine("Zeichenkette eingeben:");
                         zuErsetzen = Console.ReadLine();
                         Console.WriteLine("welches Zeichen soll ersetzt werden?");
                         zeichnen = Console.ReadLine();
                         Console.WriteLine("wodurch soll" + zeichnen+ " ersetzt werden");
-                        erzatz = Console.ReadLine();
-                        Console.WriteLine("Es wird"zeichen+ "durch" + ersatzt + "erzetzt:");
+                        ersatz = Console.ReadLine();
+                        Console.WriteLine("Es wird"+zeichnen+ "durch" + ersatz + "erzetzt:");
 
                         String ersetzt = "";
-                            for(int i=0; i< zuErsetzen.Length; i++)
-                        {
-                            if (zuErsetzen[i].ToString()==zeichen)
-                            {
-                                ersetzt += ersatz;
-                            }
-                            elso
-                            {
-                                ersetzt + -zuErsetzen[i];
-                            }
-                        }
+                        ersetzt = zuErsetzen.Replace(zeichnen, ersatz);
+                        //    for (int i=0; i< zuErsetzen.Length; i++)
+                        //    { 
+                        //    if (zuErsetzen[i].ToString()== zeichnen )
+                        //    {
+                        //        ersetzt += ersatz;
+                        //    }
+                        //    else
+
+                        //    {
+                        //        ersetzt += zuErsetzen[i];
+                        //    }
+                        //}
                         Console.WriteLine(zuErsetzen + "wird zu:" + ersetzt);
                         break;
-                    case "2":
-                Console.WriteLine("2:Vokale entfernen.");
+                    case 2:
+                        Console.WriteLine("2:Vokale entfernen.");
                         String zeichenkette;
-                        Console.WriteLine(zeichnen 
+                        Console.WriteLine("zeichenkette eingeben:");
+                        zeichenkette = Console.ReadLine();
+                        String ergebnis = "";
+
+                        ergebnis = zeichenkette.Replace("a", "");
+                        Console.WriteLine(zeichenkette + "wird zu" + ergebnis);
+
                         break;
-                    case"3":
-                Console.WriteLine("3:Beenden")
-                break;
-
-                 default;
+                    case 3:
+                        Console.WriteLine("3:programm wird beendet.");
 
 
+
+
+                     break;
+
+                    default:
+                        Console.WriteLine("Auswahl ungültig,");
+
+                        break;
                 }
                    
 
@@ -71,8 +84,8 @@ namespace strings
 
                 Console.WriteLine("neue Auswahl? (j/n)");
                 wiederholen = Console.ReadLine();
-            }while (wiederholen=="j"||wiederholen"J");
-            }
+            }while (wiederholen == "j"|| wiederholen=="J");
+            
         }
     }
 }
